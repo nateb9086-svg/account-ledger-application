10cccec764d9ae0f272234a5b8036ff7e97226a2
natefrezzell <nateb9086@gmail.com>
2026-04-27 12:09:20 -0400

    Added the Date Time Formatter and provided a user promt

--- src/main/java/pluralsight/com/Prompt.java
+++ src/main/java/pluralsight/com/Prompt.java
@@ -1,6 +1,12 @@
 package pluralsight.com;
 
-public class Deposite {
+import java.time.format.DateTimeFormatter;
+
+public class Prompt {
+    private static final String file = "transactions.csv";
+    private static final String Divider = "|";
+    private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-day");
+    private static final DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm-ss");
 
 
 
--- src/main/java/pluralsight/com/app.java
+++ src/main/java/pluralsight/com/app.java
@@ -1,4 +1,53 @@
 package pluralsight.com;
+import java.util.Scanner;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class app {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+
+
+
+
+        while (running) {
+
+            System.out.println("What action would you like?");
+            System.out.println(" [D] Add Deposit");
+            System.out.println(" [P] Make Payment");
+            System.out.println(" [L] Display Ledger");
+            System.out.println(" [X] Exit");
+
+            String choice = scanner.nextLine().trim().toUpperCase();
+
+            switch (choice) {
+                case "D":
+                    break;
+                case "P":
+                    break;
+                case "L":
+                    break;
+                case "X":
+                    System.out.println("Goodbye!;");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please c hoose D, P, L, or X.");
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
